43ad9675b0bcdd9f880ea5fb929f8915875cee50
Ilia Pohorielov <ilia.pohorielov@gmail.com>
2017-11-10 18:32:58 +0200

    make slider + text + scroll TODO: fix button

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
    $('.js-search').on('click',function () {
       $('.search-block').toggleClass('open');
    });
+    $('.main-slider').slick({
+        arrows: false,
+        dots: true,
+/*        autoplay: true,
+        autoplaySpeed: 5000*/
+    });
 });
