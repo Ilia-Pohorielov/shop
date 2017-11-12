1d5c7c362fcab466d08c1ba8ce5cc161e3403c7f
Ilia <ilia.pohorielov@gmail.com>
2017-11-12 22:00:29 +0200

    create and layout all block homepage (+ add JS parallax etc.)

--- app/js/all-plugins.js
+++ app/js/all-plugins.js
@@ -2,3 +2,7 @@
 //= jquery.min.js
 //= bootstrap.min.js
 //= slick.min.js
+//= TweenMax.min.js
+//= ScrollMagic.min.js
+//= animation.gsap.min.js
+//= jquery.mCustomScrollbar.concat.min.js
--- build/js/main.js
+++ build/js/main.js
@@ -2,17 +2,41 @@ $(document).ready(function () {
    $('.js-search').on('click',function () {
       $('.search-block').toggleClass('open');
    });
-    $('.main-slider').slick({
+    $('.top-main .js-slider').slick({
         arrows: false,
         dots: true,
         autoplay: true,
         autoplaySpeed: 5000,
         pauseOnHover: false
     });
-    $('.block-collection .collection').slick({
+    $('.block-collection .js-slider').slick({
         arrows: true,
         slidesToShow: 4,
         initialSlide: 0,
         slidesToScroll: 1
+    });
+    $('.block-instagram .js-slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        initialSlide: 0,
+        slidesToScroll: 1
+    });
+    var controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+        triggerElement: ".js-animate-parallax",
+        triggerHook: .8,
+        duration: "500%"
     })
+        .setTween(".js-animate-parallax .right", {y: "-50%", ease: Power0.easeNone})
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: ".js-animate-parallax",
+        triggerHook: .8,
+        duration: "500%"
+    })
+        .setTween(".js-animate-parallax .left", {y: "50%", ease: Power0.easeNone})
+        .addTo(controller);
+
+    $(".search-block .js-scroll").mCustomScrollbar();
 });
